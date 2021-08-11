54a9c63548717778e963dde331aad8a702ace6fe
Talank Baral <talank@gces.edu.np>
2021-08-11 16:56:00 +0545

    Used nested loop to traverse the json data

--- test/guiReport/parser.py
+++ test/guiReport/parser.py
@@ -1,6 +1,20 @@
 import json
 
+# Method that tries to find the data using nested loops
+def traverse_loop(data):
+    # logSize = len(data["tests"][0]["tests"])
+    # print(logSize)
+    
+    for i in  data["tests"][0]["tests"]:
+        # Loop level 0 where we loop inside the tets suite
+        print(str(i['name']), " has len ", str(len(i['tests'])))
 
+    # print(data["tests"][0]["tests"][1]["tests"][0]["tests"][0]["tests"][5]["tests"][0])
+
+
+
+
+# Method that tries to find the data recursively
 def traverse_recursive(data):
     if("tests" not in data):
         if("details" in data):
@@ -12,12 +26,13 @@ def traverse_recursive(data):
     else:
         # print("Recursive call********************************************************************************")
         # print(data["tests"])
+        # item = next(iter(filter(lambda x: x['name'] == name, items)), None)
         print(traverse_recursive(data["tests"]))
     
 
 
 # Opening JSON file
-f = open('results.json',)
+f = open('test.json',)
   
 # returns JSON object as 
 # a dictionary
@@ -26,8 +41,10 @@ data = json.load(f)
 # Iterating through the json
 # list
 # for i in data['tests']:
-#     print(i)
-traverse_recursive(data)
-  
+    # print(i)
+# traverse_recursive(data)
+traverse_loop(data)
+
+
 # Closing file
 f.close()
