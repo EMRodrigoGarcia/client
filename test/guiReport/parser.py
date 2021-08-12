bbebd20e3c862c430633f7fd28e4937f2f8b3c33
Talank Baral <talank@gces.edu.np>
2021-08-12 14:07:29 +0545

    Traverse through result file to get only the failing scenarios

--- test/guiReport/parser.py
+++ test/guiReport/parser.py
@@ -2,47 +2,29 @@ import json
 
 # Method that tries to find the data using nested loops
 def traverse_loop(data):
-    # logSize = len(data["tests"][0]["tests"])
-    # print(logSize)
+    failing_tests = []
     
     for feature_file in  data["tests"][0]["tests"]:
         # Loop level 0 where we loop inside the tets suite
-        print(str(feature_file['name']), " has len ", str(len(feature_file['tests'])))
+        # print(str(feature_file['name']), " has len ", str(len(feature_file['tests'])))
         for feature in feature_file['tests']:
-            print(str(feature['name']), " has len ", str(len(feature['tests'])))
+            # print(str(feature['name']), " has len ", str(len(feature['tests'])))
             for scenario in feature['tests']:
                 if("tests" not in scenario): break
-                print(str(scenario['name']), " has len ", str(len(scenario['tests'])))
+                # print(str(scenario['name']), " has len ", str(len(scenario['tests'])))
                 for test_step in scenario['tests']:
                     if("tests" not in test_step): break
-                    print(str(test_step['name']), " has len ", str(len(test_step['tests'])))
-                    print("Recursive call********************************************************************************")
+                    for error in test_step['tests']:
+                        test = {"Feature_file": str(feature_file['name']),
+                        "Feature": str(feature['name']),
+                        "Scenario": str(scenario['name']),
+                        "Test Step": str(test_step['name']),
+                        "Error Details": str(error['detail'])
+                        }
+                        failing_tests.append(test)
 
+    return failing_tests
 
-
-
-
-
-    # print(data["tests"][0]["tests"][1]["tests"][0]["tests"][0]["tests"][5]["tests"][0])
-
-
-
-
-# Method that tries to find the data recursively
-def traverse_recursive(data):
-    if("tests" not in data):
-        if("details" in data):
-            return data["details"]
-        elif("name" in data): 
-            return data["name"]
-        else:
-            return "No name or details available."
-    else:
-        # print("Recursive call********************************************************************************")
-        # print(data["tests"])
-        # item = next(iter(filter(lambda x: x['name'] == name, items)), None)
-        print(traverse_recursive(data["tests"]))
-    
 
 
 # Opening JSON file
@@ -52,12 +34,9 @@ f = open('test.json',)
 # a dictionary
 data = json.load(f)
   
-# Iterating through the json
-# list
-# for i in data['tests']:
-    # print(i)
-# traverse_recursive(data)
-traverse_loop(data)
+# Traverse through the json file to look for failing tests
+failing_tests = traverse_loop(data)
+print(failing_tests)
 
 
 # Closing file
