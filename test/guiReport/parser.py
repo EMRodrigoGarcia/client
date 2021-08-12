9bcea2eab2511527f118db05ac509bc067dd9a20
Talank Baral <talank@gces.edu.np>
2021-08-12 15:16:45 +0545

    Added comments and beautify output

--- test/guiReport/parser.py
+++ test/guiReport/parser.py
@@ -1,34 +1,43 @@
 import json
+import sys
 
 # Method that tries to find the data using nested loops
 def traverse_loop(data):
     failing_tests = []
     
+    # Loop level 0 where we loop through all the feature files inside the only test suites that we have.
     for feature_file in  data["tests"][0]["tests"]:
-        # Loop level 0 where we loop inside the tets suite
-        # print(str(feature_file['name']), " has len ", str(len(feature_file['tests'])))
+
+        # Loop through all the features in the feature file
         for feature in feature_file['tests']:
-            # print(str(feature['name']), " has len ", str(len(feature['tests'])))
+
+            # Loop through all the scenarios in the feature
             for scenario in feature['tests']:
+
+                # If the scenario is not skipped, then loop through all the steps in the scenario
                 if("tests" not in scenario): break
-                # print(str(scenario['name']), " has len ", str(len(scenario['tests'])))
                 for test_step in scenario['tests']:
+
+                    # If the test step fails then it contains further "tests" object
+                    # So loop through all the errors in the test step
                     if("tests" not in test_step): break
                     for error in test_step['tests']:
-                        test = {"Feature_file": str(feature_file['name']),
+
+                        # Append the information of failing tests into the list of failing tests
+                        test = {"Feature File": str(feature_file['name']),
                         "Feature": str(feature['name']),
                         "Scenario": str(scenario['name']),
                         "Test Step": str(test_step['name']),
                         "Error Details": str(error['detail'])
                         }
+
                         failing_tests.append(test)
 
     return failing_tests
 
 
-
 # Opening JSON file
-f = open('test.json',)
+f = open(str(sys.argv[1]))
   
 # returns JSON object as 
 # a dictionary
@@ -36,8 +45,8 @@ data = json.load(f)
   
 # Traverse through the json file to look for failing tests
 failing_tests = traverse_loop(data)
-print(failing_tests)
-
+# print(failing_tests)
+print(json.dumps(failing_tests, indent=4, sort_keys=True))
 
 # Closing file
 f.close()
